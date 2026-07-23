0d2cc4e112bc764deda8cbf91851857e3318238a
yahirhidalgodev <yahir.hidalgo.dev@gmail.com>
2026-07-22 18:37:14 -0600

    feat: implementar manejo global de excepciones para errores de validación

---
+++ src/main/java/com/yahir/primer_api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.yahir.primer_api.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+@ExceptionHandler(MethodArgumentNotValidException.class)
+public ResponseEntity<Map<String, String>> manejarErroresValidacion(MethodArgumentNotValidException ex){
+
+    Map<String, String> errores = new HashMap<>();
+
+    ex.getBindingResult()
+            .getFieldErrors()
+            .forEach(error ->
+                    errores.put(error.getField(), error.getDefaultMessage()));
+
+    return ResponseEntity.badRequest().body(errores);
+}
+
+
+}
